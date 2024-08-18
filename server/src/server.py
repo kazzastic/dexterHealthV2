7fa998578b5c7418d8e2a69c166832f57c136110
kazzastic <rkazim79@gmail.com>
2024-08-18 23:33:45 +0200

    and now the passwords are hashed in the database

--- server/src/server.py
+++ server/src/server.py
@@ -7,6 +7,7 @@ from database import SessionLocal
 from sqlalchemy.orm import Session
 from models.User import User
 from models.Chat import Chat
+import bcrypt
 
 @dataclass 
 class ConnectionManager:
@@ -89,8 +90,12 @@ async def register_user(user: UserCreate, db: Session = Depends(get_db)):
     existing_user = db.query(User).filter(User.username == user.username).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Username already exists")
+    
+    # Hash the password
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
 
-    new_user = User(username=user.username, password = user.password)
+
+    new_user = User(username=user.username, password = hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
